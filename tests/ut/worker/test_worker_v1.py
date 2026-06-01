0e37cadc61370701a2d21d6cf893c74e5f1e7d59
yjyang62 <yangjinyang5@huawei.com>
2026-06-01 16:50:55 +0800

    cleancode
    Signed-off-by: yjyang62 <yangjinyang5@huawei.com>

--- tests/ut/worker/test_worker_v1.py
+++ tests/ut/worker/test_worker_v1.py
@@ -1044,7 +1044,7 @@ class TestNPUWorker(TestBase):
         # Create worker mock
         with (
             patch.object(NPUWorker, "__init__", lambda x, **kwargs: None),
-            patch("vllm_ascend.worker.worker.ensure_kv_transfer_initialized") as mock_ensure_kv_transfer,
+            patch("vllm_ascend.worker.worker.ensure_kv_transfer_initialized"),
         ):
             worker = NPUWorker()
             worker.model_runner = MagicMock()
@@ -1066,7 +1066,6 @@ class TestNPUWorker(TestBase):
             worker.initialize_from_config(mock_kv_cache_config)
 
             # Verify calls
-            mock_ensure_kv_transfer.assert_called_once_with(worker.vllm_config, mock_kv_cache_config)
             mock_allocator_class.get_instance.assert_called_once()
             mock_allocator.use_memory_pool.assert_called_once_with(tag="kv_cache")
             worker.model_runner.initialize_kv_cache.assert_called_once_with(mock_kv_cache_config)
@@ -1140,7 +1139,7 @@ class TestNPUWorker(TestBase):
         # Create worker mock
         with (
             patch.object(NPUWorker, "__init__", lambda x, **kwargs: None),
-            patch("vllm_ascend.worker.worker.ensure_kv_transfer_initialized") as mock_ensure_kv_transfer,
+            patch("vllm_ascend.worker.worker.ensure_kv_transfer_initialized"),
         ):
             worker = NPUWorker()
             worker.model_runner = MagicMock()
